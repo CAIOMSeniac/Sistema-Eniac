22da85ae62958ba6cac9c9fcea0660225105aaa0
caio <caio.saes@upd8.com.br>
2022-09-18 15:13:09 -0300

    dash dados

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,18 @@
 
 session_start();
 include_once('conexao_Banco.php');
-
+function consulta_usuarios($condicao){
+    $servidor = 'localhost';
+    $usuario = 'root';
+    $senha = '';
+    $banco = 'colegio';
+    $conn = new mysqli($servidor, $usuario, $senha, $banco);
+    if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
+    $querySql = "SELECT * FROM  `usuarios` WHERE ".$condicao."";
+    $result = mysqli_query($conn,$querySql);
+    $row = mysqli_num_rows($result);
+    return $row;
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -10,6 +21,8 @@ include_once('conexao_Banco.php');
     <!-- LINKS DO BOOTSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <!--Google Graph-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <!---->
     <link rel="stylesheet" type="text/css" href="styleGen.css">
     <link rel="icon" type="imagem/png" href="https://portalacademico.eniac.edu.br/pluginfile.php/1/theme_snap/favicon/1663305488/favicon-32.png" />
@@ -32,5 +45,13 @@ include_once('conexao_Banco.php');
     <a href='imagens.php' target='_blank'>imagens</a>
     <br>
     <a href='logout.php'>logout</a>
+<br><br>
+<?php
+echo "<h5>perfis que:</h5>";
+echo "<h5> tem o perfil ativo ".consulta_usuarios('`ativo` = 1')."</h5>";
+echo "<h5> tem o perfil desativo ".consulta_usuarios('`ativo` = 0')."</h5>";
+echo "<h5> tem a funcao Adm ".consulta_usuarios('`funcao` = "Adm"')."</h5>";
+echo "<h5> tem a funcao User ".consulta_usuarios('`funcao` = "User"')."</h5>";
+?>
 </body>
 </html>
